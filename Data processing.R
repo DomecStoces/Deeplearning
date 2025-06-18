2391e7e5df0d5ca91d435a1e622b3a21e8ce7350
DomecStoces <dominik.stoces@mendelu.cz>
2025-06-18 16:03:28 +0200

    actualization of protocol

--- Data processing.R
+++ Data processing.R
@@ -1,7 +1,7 @@
 library(data.table)
 
 # Read all rows as character to process headers manually
-raw <- fread("C:/Users/Dominik/Desktop/Test1-Dominik-2025-06-15/labeled-data/output/image_predictions_DLC_Resnet50_Test1Jun15shuffle1_snapshot_010.csv", header = FALSE)
+raw <- fread("C:/Users/Dominik/Desktop/Test1-Dominik-2025-06-15/labeled-data/output/image_predictions_DLC_Resnet101_Test1Jun15shuffle1_snapshot_240.csv", header = FALSE)
 
 # Extract header info
 colnames <- paste(raw[2], raw[3], raw[4], sep = "_")
