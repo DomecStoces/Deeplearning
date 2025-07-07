5a72dcd3010059aa98f45d04c6c2591c9727db5b
DomecStoces <dominik.stoces@mendelu.cz>
2025-07-07 21:05:18 +0200

    ch.

--- FA_protocol.R
+++ FA_protocol.R
@@ -4,10 +4,10 @@ library(writexl)
 
 # Read .xlsx dataset with header
 dataset <- read_xlsx("data_picipennis.xlsx", col_names = TRUE)
-grubb_picipennis3 <- read_xlsx("grubb_picipennis.xlsx", col_names = TRUE)
+
 # Convert to data.table
 dataset_dt <- as.data.table(dataset)
-grubb_picipennis4 <- as.data.table(grubb_picipennis3)
+
 ### FA protocol ###
 # Drop NAs
 library(dplyr)
@@ -21,7 +21,7 @@ skewness(grubb_picipennis$a1)
 kurtosis(grubb_picipennis$a1)
 grubbs.test(grubb_picipennis$a1)
 
-skewness(clean_data$a1)
+skewness(data_picipennis$a1)
 kurtosis(clean_data$a1)
 grubbs.test(clean_data$a1)
 
@@ -31,25 +31,59 @@ summary(aov_Ophonus)
 
 # Dependency on Directional Asymmetry
 t.test(dataset_dt$DA.a1)
+t.test(data_picipennis$DA.a1)
 
 # Normality of raw dataset in grubb_XXX
 shapiro.test(grubb_picipennis$a1)
+# Normality of FA index
+shapiro.test(data_picipennis$FA.a1)
 
 # Homogenity of variance of FA index
 library(car)
 leveneTest(FA.a1~Treatment*Sex, data = dataset_dt)
+leveneTest(FA.a1~Treatment*Sex, data = data_picipennis)
 
+leveneTest(FA.a1~Treatment*Wing.m., data = dataset_dt)
+leveneTest(FA.a1~Treatment*Wing.m., data = data_picipennis)
 # Dependency |R-L| to Body size
-dataset_dt$a1_abs <- abs(dataset_dt$`a1 R` - dataset_dt$`a1 L`) 
-lm_abs <- lm(a1_abs ~ Body.size, data = dataset_dt)
+data_picipennis$a1_abs <- abs(data_picipennis$`a1 R` - data_picipennis$`a1 L`) 
+lm_abs <- lm(a1_abs ~ Body.size, data = data_picipennis)
 summary(lm_abs)
 
 # Dependency on Sex:Wing morphology
-lm_sex <- lm(FA.a1 ~ Sex*Wing, data = dataset_dt)
+lm_sex <- lm(FA.a1 ~ Sex*Wing, data = data_picipennis)
 summary(lm_sex)
 
 library(lme4)
-mod1<-lm(FA.a1~Treatment*Wing+Sex+Month,data= dataset_dt)
+mod1<-lm(FA.a1~Body.size+Treatment*Wing+Sex+Month,data= data_picipennis)
 summary(mod1)
 aov1<-Anova(mod1)
 aov1
+
+library(ggplot2)
+library(ggpubr)
+
+d<-ggplot(data_picipennis, aes(x = Treatment, y = FA.a1, fill = Treatment)) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.6) +
+  geom_jitter(aes(color = Treatment), width = 0.2, size = 1.5, alpha = 0.8) +
+  facet_wrap(~ Wing.m.) +
+  scale_fill_grey(start = 0.3, end = 0.8) +
+  scale_color_grey(start = 0.3, end = 0.8) +
+  labs(
+    title = "FA.a1 across Treatments by Wing morph",
+    x = "Treatment",
+    y = "Fluctuating Asymmetry (FA.a1)",
+    fill = "Treatment",
+    color = "Treatment"
+  ) +
+  theme_bw(base_size = 12) +
+  stat_compare_means(
+    method = "wilcox.test",                            
+    comparisons = list(c("Control", "Solar")),   
+    label = "p.format",                          
+    hide.ns = FALSE)
+d
+# Save the plot
+tiff('Harpalus_picipennis.tiff',units="in",width=7,height=6,bg="white",res=600)
+d
+dev.off()
