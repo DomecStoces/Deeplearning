695520aa8e089ef3d181009b9c2e2e79946c9c09
DomecStoces <dominik.stoces@mendelu.cz>
2025-07-28 15:04:15 +0200

    ch.

--- FA_protocol.R
+++ FA_protocol.R
@@ -34,12 +34,19 @@ grubb_picipennis19$outlier_type <- with(grubb_picipennis19, ifelse(
 ))
 table(grubb_picipennis19$outlier_type)
 
-skewness(grubb_picipennis19$a1)
-kurtosis(grubb_picipennis19$a1)
+library(dplyr)
+grubb_picipennis19_clean <- subset(grubb_picipennis19, outlier_type == "none")
+IDs_no_outliers <- unique(grubb_picipennis19_clean$Group[grubb_picipennis19_clean$outlier_type == "none"])
+data_picipennis19_clean <- data_picipennis19 %>%
+  filter(ID %in% IDs_no_outliers)
+str(data_picipennis19_clean)
+
+skewness(grubb_picipennis19_clean$a1)
+kurtosis(grubb_picipennis19_clean$a1)
 
 # Dependency on ME in raw dataset in grubb_XXX; extract variance and correlation components
 library(nlme)
-lme_model <- lme(a1 ~ SIDE.a1, random = ~1|Group/SIDE.a1, data = grubb_picipennis19)
+lme_model <- lme(a1 ~ SIDE.a1, random = ~1|Group/SIDE.a1, data = grubb_picipennis19_clean)
 summary(lme_model)
 VarCorr(lme_model)
 
@@ -49,84 +56,62 @@ summary(aov1)
 # A linear mixed-effects model (REML) was used to test for directional asymmetry (DA), fluctuating asymmetry (FA), and measurement error (ME). The fixed effect of side was not significant (p = 0.33), indicating no DA. The variance attributable to individual × side interaction (FA) was 0.000175, while residual variance (ME) was 0.00000052, yielding a %ME of 0.30%.
 # Palmer, A. R., & Strobeck, C. (2003). Fluctuating asymmetry analyses revisited. In Polak, M. (Ed.), Developmental Instability: Causes and Consequences. Oxford University Press, pp. 279–319.
 
-# Dependency on Directional Asymmetry
-t.test(dataset_dt$DA.a1)
-t.test(data_picipennis1$DA.a1)
+# Dependency |R-L| to Body size
+data_picipennis19_clean$a1_abs<- abs(data_picipennis19_clean$`a1 R` - data_picipennis19_clean$`a1 L`) 
+lm_abs <- lm(a1_abs ~ Body.size, data = data_picipennis19_clean)
+summary(lm_abs)
 
-# Normality of FA index; multiplicative error structure in FA
-shapiro.test(data_picipennis19$FA)
+# Normality of |R-L|; i) if significant -> error structure is non-additive; ii) test log transformation if significant -> non-multiplicative
+shapiro.test(data_picipennis19_clean$a1_abs)
+
+data_picipennis19_clean$log_fa <- log(data_picipennis19_clean$a1_abs + 0.0001)
+shapiro.test(data_picipennis19$log_fa)
 
 # Homogenity of variance of FA index
 library(car)
-leveneTest(FA.a1~Treatment*Sex, data = dataset_dt)
-leveneTest(FA~Treatment*Sex, data = data_picipennis19)
+leveneTest(FA3~Treatment*Sex, data = data_picipennis19_clean)
 
-leveneTest(FA.a1~Treatment*Wing.m., data = dataset_dt)
-leveneTest(FA~Treatment*Wing.m., data = data_picipennis19)
-# Dependency |R-L| to Body size
-data_picipennis19$a1_abs<- abs(data_picipennis19$`a1 R` - data_picipennis19$`a1 L`) 
-lm_abs <- lm(a1_abs ~ Body.size, data = data_picipennis19)
-summary(lm_abs)
-
-data_picipennis_m <- data_picipennis1[data_picipennis1$Month == 5, ]
-
-data_picipennis_m$a1_abs <- abs(data_picipennis_m$`a1 R` - data_picipennis_m$`a1 L`) 
-lm_abs <- lm(a1_abs ~ Body.size, data = data_picipennis_m)
-summary(lm_abs)
-
-lm_size<-lm(FA.a1~Body.size+Sex*Wing,data_picipennis_m)
-summary(lm_size)
+leveneTest(FA3~Treatment*Wing.m., data = data_picipennis19_clean)
 
 # Dependency on Sex:Wing morphology
-lm_sex <- lm(FA ~ Sex*Wing, data = data_picipennis19)
+lm_sex <- lm(FA3 ~ Sex*Wing, data = data_picipennis19_clean)
 summary(lm_sex)
-
+lm_treat <- lm(FA3 ~ Treatment*Wing, data = data_picipennis19_clean)
+summary(lm_treat)
+lm_st <- lm(FA3 ~ Treatment*Sex, data = data_picipennis19_clean)
+summary(lm_st)
 library(lme4)
-mod1<-lmer(FA~Body.size+Treatment * Wing + Sex + (1 | ID),data= data_picipennis19)
+mod1<-lmer(FA3~Body.size+Treatment * Wing + Sex + (1 | ID),data= data_picipennis19)
 summary(mod1)
-aov1<-Anova(mod1,type=2,adjust="tukey")
-aov1
+library(lmerTest)
+anova(mod1)
+# When |R-L
+library(glmmTMB)
+mod_lognormal <- glmmTMB(FA3 ~ Body.size + Treatment * Wing + Sex + (1|ID),
+                         data = data_picipennis19,
+                         family = gaussian(link = "log"))
+summary(mod_lognormal)
+library(DHARMa)
+simres <- simulateResiduals(mod_lognormal)
+plot(simres)
+library(car)
+Anova(mod_lognormal, type = 3)
 
-library(brms)
-mod1_brm <- brm(
-  formula = FA ~ Body.size+Treatment * Wing + Sex + (1 | ID),
-  data = data_picipennis19, family=gaussian(),
-  chains = 4,           
-  cores = 4,            
-  iter = 5000,        
-  seed = 123,save_pars = save_pars(all = TRUE))
+tiff('DHARMa_residual_HP_a1.tiff',units="in",width=7,height=6,bg="white",res=600)
+plot(simres)
+dev.off()
 
-summary(mod1_brm)
+library(emmeans)
 
-pp_check(mod1_brm)
-pp_check(mod1_brm, type = "stat", stat = "kurtosis")
-loo(mod1_brm,moment_match = TRUE)
-
-library(tidybayes)
-conditional_effects(mod1_brm, "Treatment:Wing")
-library(bayesplot)
-mcmc_plot(mod1_brm, 
-          variables = variables(mod1_brm, "^b_"), 
-          prob = 0.95, 
-          prob_outer = 0.99, 
-          type = "intervals")
-
-# Extract predicted values
-y_pred <- posterior_predict(mod1_brm)
-y_obs <- mod1_brm$data$FA
-
-# Compute residuals (use the mean across posterior draws)
-residuals_mean <- apply(y_pred, 2, mean) - y_obs
-
-# Basic QQ plot
-qqnorm(residuals_mean)
-qqline(residuals_mean)
+mod_3 <- brm(FA3~Body.size + Treatment * Wing + Sex + (1|ID),
+             data = data_picipennis19,
+             family = gaussian(link = "log"))
 
 library(ggplot2)
 library(ggpubr)
 
 # Treatment with Wing morphology
-d<-ggplot(data_picipennis15, aes(x = Treatment, y = FA, fill = Treatment)) +
+d<-ggplot(data_picipennis19, aes(x = Treatment, y = FA3, fill = Treatment)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.6) +
   geom_jitter(aes(color = Treatment), width = 0.2, size = 1.5, alpha = 0.8) +
   facet_wrap(~ Wing.m.) +
@@ -152,7 +137,7 @@ d
 dev.off()
 
 # Treatment with Sex
-de<-ggplot(data_picipennis1, aes(x = Sex, y = FA.a1, fill = Sex)) +
+de<-ggplot(data_picipennis19, aes(x = Sex, y = FA.a1, fill = Sex)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.6) +
   geom_jitter(aes(color = Sex), width = 0.2, size = 1.5, alpha = 0.8) +
   facet_wrap(~ Treatment) +
@@ -178,7 +163,7 @@ de
 dev.off()
 
 # Wing morphology across Sex
-df<-ggplot(data_picipennis1, aes(x = Sex, y = FA.a1, fill = Sex)) +
+df<-ggplot(data_picipennis19, aes(x = Sex, y = FA3, fill = Sex)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.6) +
   geom_jitter(aes(color = Sex), width = 0.2, size = 1.5, alpha = 0.8) +
   facet_wrap(~ Wing.m.) +
